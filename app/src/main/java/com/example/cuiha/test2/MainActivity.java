2350a9767bfd6a689ea78f399d0c8a3747db87ec
cuiha <2554803008@qq.com>
2018-04-09 20:34:57 +0800

    test2

---
+++ app/src/main/java/com/example/cuiha/test2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cuiha.test2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"第一个页面",Toast.LENGTH_SHORT).show();
+    }
+}

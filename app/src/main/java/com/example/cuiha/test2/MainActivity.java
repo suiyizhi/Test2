6e44101bb5fcc16ad427360a7ec3aa026ba5e847
cuiha <2554803008@qq.com>
2018-04-10 10:39:54 +0800

    test2

--- app/src/main/java/com/example/cuiha/test2/MainActivity.java
+++ app/src/main/java/com/example/cuiha/test2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"第一个页面",Toast.LENGTH_SHORT).show();
+
     }
 }
